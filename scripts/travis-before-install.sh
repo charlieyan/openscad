4635a220b3da12c9a338000d8b2f2251f40dafae
Torsten Paul <Torsten.Paul@gmx.de>
2016-09-20 22:54:18 +0200

    Change installation order.

--- scripts/travis-before-install.sh
+++ scripts/travis-before-install.sh
@@ -44,28 +44,30 @@ PACKAGES='
 	lib3mf-dev
 '
 
-if [[ "$DIST" == "trusty" ]]
-then
-	echo "Adding external repositories for $DIST"
-	echo 'yes' | "$SUDO" add-apt-repository 'deb http://download.opensuse.org/repositories/home:/t-paul:/lib3mf/xUbuntu_14.04/ ./'
-elif [[ "$DIST" == "precise" ]]
-then
-	echo "Adding external repositories for $DIST"
-	echo 'yes' | "$SUDO" add-apt-repository 'deb http://download.opensuse.org/repositories/home:/t-paul:/lib3mf/xUbuntu_12.04/ ./'
-	echo 'yes' | "$SUDO" add-apt-repository ppa:chrysn/openscad
-	echo 'yes' | "$SUDO" add-apt-repository ppa:mapnik/nightly-trunk
-	echo 'yes' | "$SUDO" add-apt-repository ppa:oibaf/graphics-drivers
-
-	EXTRA_PACKAGES=libgl1-mesa-dev-lts-quantal
-else
-	echo "No external repositories for unknown distribution '$DIST'"
-fi
-
 # Purge fglrx driver if that is installed by default
 # as this contains a libGL that breaks the GLX extension
 # in Xvfb causing all graphical tests to fail.
 "$SUDO" apt-get update "$QUIET"
 "$SUDO" apt-get purge "$QUIET" fglrx
 
-"$SUDO" apt-get install "$QUIET" $PACKAGES
-"$SUDO" apt-get install "$QUIET" --install-recommends $EXTRA_PACKAGES
+if [[ "$DIST" == "trusty" ]]
+then
+	echo "Adding external repositories for $DIST"
+	echo 'yes' | "$SUDO" add-apt-repository 'deb http://download.opensuse.org/repositories/home:/t-paul:/lib3mf/xUbuntu_14.04/ ./'
+
+elif [[ "$DIST" == "precise" ]]
+then
+	echo "Adding external repositories for $DIST"
+	echo 'yes' | "$SUDO" add-apt-repository ppa:chrysn/openscad
+	echo 'yes' | "$SUDO" add-apt-repository ppa:mapnik/nightly-trunk
+	echo 'yes' | "$SUDO" add-apt-repository 'deb http://download.opensuse.org/repositories/home:/t-paul:/lib3mf/xUbuntu_12.04/ ./'
+
+	"$SUDO" apt-get update "$QUIET"
+	"$SUDO" apt-get install "$QUIET" $PACKAGES
+
+	echo 'yes' | "$SUDO" add-apt-repository ppa:oibaf/graphics-drivers
+	"$SUDO" apt-get update "$QUIET"
+	"$SUDO" apt-get install "$QUIET" --install-recommends libgl1-mesa-dev-lts-quantal
+else
+	echo "WARNING: No install recipe for distribution '$DIST'"
+fi
